adfc6aab7cbc9d55928a42a10d3b835b578bd5cd
laurattt <lauratoro422@gmail.com>
2025-09-22 16:18:31 +0200

    Initial commit

---
+++ app/src/main/java/com/lauratoro/myapplication/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.lauratoro.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_main) // --> LINEA IMPORTANTE, crea la aplicación según el xml
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<Button>(/* id = */ R.id.button2)
+            .setOnClickListener {
+                Log.d("BUTTONS", "button2 press")
+            }
+        
+
+
+
+    }
+}
